716dce5abd61132127933db05d48b04072587315
awad3317 <127610663+awad3317@users.noreply.github.com>
2024-09-26 18:56:00 +0300

    add project

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('head.html');?>
+<body>
+    <div class="container">
+        <?php include('navbar.html');?>
+        <?php include('header.html');?>
+        <?php include('customer_favorites.html');?>
+        <?php include('special_dishes.html');?>
+
+        <?php include('testimonials.html');?>
+        <?php include('our_story&services.html');?>
+        <?php include('footer.html'); ?>
+    </div>
+    <script src="js/popper.min.js"></script>
+    <script src="js/jquery-3.7.1.min.js"></script>
+    <script src="js/bootstrap.js"></script>
+</body>
+</html>
